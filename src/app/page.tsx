a9e79ab43de11e9984fbb253fe8c22489cee08c0
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-07-05 11:19:23 +0530

    button template customizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">
-        Hello World
-      </p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">
+            I am a Button
+          </Button>
+        </div>
+      <div>
+      <Input placeholder="I am an Input" /> 
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="I am a Text Area" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
+    </div>
     </div>
   );
 };
